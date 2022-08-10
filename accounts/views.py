d3b3b5680c23ce75f8e624307ff45ceb380e21cf
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 10:13:52 +0430

    Importing relevant packages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Create your views here.
