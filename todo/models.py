21b37713da135efcb062eceac8dad973cc20641e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 10:19:16 +0430

    Implementing Task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    """
+    Task model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        order_with_respect_to = 'user'
+        ordering = ['-created_at']
