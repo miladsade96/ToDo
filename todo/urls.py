3f7716cc5eb132da82ca20460450ae427938b2e9
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 11:29:59 +0430

    Defining todo urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path
 from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskCompleteView
+
+app_name = 'todo'
+
+urlpatterns = [
+    path('', TaskListView.as_view(), name='todo_list'),
+    path('create/', TaskCreateView.as_view(), name='todo_create'),
+    path('update/<int:pk>/', TaskUpdateView.as_view(), name='todo_update'),
+    path('delete/<int:pk>/', TaskDeleteView.as_view(), name='todo_delete'),
+    path('complete/<int:pk>/', TaskCompleteView.as_view(), name='todo_complete'),
+]
