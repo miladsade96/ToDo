c225a9ee85013e602bcce29c02ee2c9c49add1d0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 11:48:04 +0430

    Registering task model on Admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
+
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at', 'completed')
+    list_filter = ('completed',)
+    search_fields = ('title',)
+    ordering = ('-created_at',)
+    fields = ('title', 'completed')
