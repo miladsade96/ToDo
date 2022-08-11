6a8bc33952f7b463b8e3c9b5f7ef12fd674c896c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 10:59:27 +0430

    Importing relevant modules

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .models import Task
+from django.views import View
+from .forms import TaskUpdateForm
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
