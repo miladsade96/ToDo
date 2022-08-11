dde4c25bdadf8ba2d54490c3d19c898e38667a3e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 11:06:14 +0430

    Implementing TaskListView

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'todo/task_list.html'
+    context_object_name = 'tasks'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
