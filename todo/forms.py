b517bafd930cd049142ed6c4e528756b0da84f51
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-11 10:57:19 +0430

    Implementing TaskUpdateForm

--- todo/forms.py
+++ todo/forms.py
@@ -1,2 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField, TextInput
 from .models import Task
+
+
+class TaskUpdateForm(ModelForm):
+    """
+    Form for updating a task.
+    """
+    title = CharField(
+        widget=TextInput(attrs={
+            'class': 'form-control-rounded-4', 'placeholder': 'Title', 'name': 'title'}),
+        label='')
+
+    class Meta:
+        model = Task
+        fields = ('title',)
