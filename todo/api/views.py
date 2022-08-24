115f58f34b82bcad73c7c93c644e23abcefb9b17
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 23:15:45 +0430

    Implementing TodoListAPIView

--- todo/api/views.py
+++ todo/api/views.py
@@ -4,3 +4,20 @@ from .serializers import TaskSerializer
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+
+
+class TodoListAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
