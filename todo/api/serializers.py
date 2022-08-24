54faae6eb91629fb5e91cc98540373834fc52f5a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 23:11:25 +0430

    Implementing task serializer

---
+++ todo/api/serializers.py
@@ -0,0 +1,12 @@
+from ..models import Task
+from rest_framework import serializers
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    """
+        Task model serializer class
+    """
+    class Meta:
+        model = Task
+        fields = ['id', 'user', 'title', 'completed', 'created_at', 'updated_at']
+        read_only_fields = ["user"]
