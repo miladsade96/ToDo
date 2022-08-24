916c5d8f480ba700dfc8cfeabfe4421ca1b6ce06
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 23:09:12 +0430

    Adding auth feature to browsable api

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
     path('accounts/', include('accounts.urls')),
+    path("api-auth/", include("rest_framework.urls"))
 ]
