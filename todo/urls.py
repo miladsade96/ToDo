87864f9c04790a68a7a9cfcc387ea680c46a917e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 23:09:49 +0430

    Defining todo api url branch

--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TaskCompleteView
 
 app_name = 'todo'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('update/<int:pk>/', TaskUpdateView.as_view(), name='todo_update'),
     path('delete/<int:pk>/', TaskDeleteView.as_view(), name='todo_delete'),
     path('complete/<int:pk>/', TaskCompleteView.as_view(), name='todo_complete'),
+    path('api/', include('todo.api.urls'))
 ]
