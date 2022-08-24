30eb413b73985e527ce86e6b871a9cf9ff61ab52
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 23:08:30 +0430

    Fixing a minor issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(next_page="/"), name='logout'),
+    path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 ]
