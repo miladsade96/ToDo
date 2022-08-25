1ddffada0f42a76f2144904dfef33cb1a358fca7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-25 12:43:35 +0430

    Importing relevant modules

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 from rest_framework.documentation import include_docs_urls
+
 
 
 urlpatterns = [
