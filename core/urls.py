1ce797ee9363665159cc64c06d879b1f7fbcf5ab
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-25 12:45:46 +0430

    Defining schema_view

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,18 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo API",
+      default_version='v1',
+      description="ToDo App API Documentation",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="EverLookNeverSee@Proton.Me"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
