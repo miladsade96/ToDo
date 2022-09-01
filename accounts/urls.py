7256609423facb8cd85a2615f4735d1d6ad4b8ea
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-01 14:03:41 +0430

    Defining api url branch

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, RegisterPage
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
+    path('api/', include('accounts.api.urls'))
 ]
