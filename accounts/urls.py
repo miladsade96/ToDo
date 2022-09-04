83cca7e95738e77cc864bfb664e1310667ae1ba1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-04 21:50:12 +0430

    Defining v1 and v2 url branches

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
-    path('api/', include('accounts.api.urls'))
+    path('api/v1/', include('accounts.api.v1.urls')),
+    path('api/v2/', include('accounts.api.v2.urls'))
 ]
