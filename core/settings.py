c05898b70742e9e704ae952329fb51e221e25a58
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-09 15:12:28 +0430

    Updating installed apps

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'drf_yasg',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'mail_templated'
 ]
 
 MIDDLEWARE = [
