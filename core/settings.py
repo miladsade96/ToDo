c053f3fa48198a0d422938643b55a91cfd451736
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-09 15:12:59 +0430

    Adding default authentication classes

--- core/settings.py
+++ core/settings.py
@@ -138,4 +138,10 @@ LOGIN_URL = 'accounts/login'
 # restframework configuration
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
 }
