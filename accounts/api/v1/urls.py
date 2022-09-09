1cec7696431b6f0bcfd26f8284fe7d14a7568701
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-09 15:14:14 +0430

    Creating urls for api v1

---
+++ accounts/api/v1/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+
+app_name = "api-v1"
+
+
+urlpatterns = [
+    # --- accounts ---
+    # Registration
+    path("registration/", views.RegistrationAPIView.as_view(), name="registration"),
+    # # Activation
+    # path("activation/confirm/<str:token>", views.ActivationAPIView.as_view(), name="activation"),
+    # # Resend activation
+    # path("activation/resend/", views.ActivationResendAPIView.as_view(), name="activation_resend"),
+    # # Password change
+    # path("change-password/", views.ChangePasswordAPIView.as_view(), name="change_password"),
+    # # Token
+    # path("token/login/", CustomAuthToken.as_view(), name="token_login"),
+    # path("token/logout/", CustomDiscardAuthToken.as_view(), name="token_logout"),
+    # # JWT
+    # path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt_create"),
+    # path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+    # path("jwt/verify/", TokenVerifyView.as_view(), name="jwt_verify")
+]
