e81cc54e0d008f9de0cd9bbffcba75b9ffbedb1e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-12 13:32:06 +0430

    Enabling change password url path

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -10,12 +10,15 @@ urlpatterns = [
     # --- accounts ---
     # Registration
     path("registration/", views.RegistrationAPIView.as_view(), name="registration"),
+
     # Activation
     path("activation/confirm/<str:token>", views.ActivationAPIView.as_view(), name="activation"),
-    # # Resend activation
+    # Resend activation
     path("activation/resend/", views.ActivationResendAPIView.as_view(), name="activation_resend"),
-    # # Password change
-    # path("change-password/", views.ChangePasswordAPIView.as_view(), name="change_password"),
+
+    # Change password
+    path("change-password/", views.ChangePasswordAPIView.as_view(), name="change_password"),
+
     # # Token
     # path("token/login/", CustomAuthToken.as_view(), name="token_login"),
     # path("token/logout/", CustomDiscardAuthToken.as_view(), name="token_logout"),
