66c4aed3799ab2c0c17a15b70938325b67224008
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-14 12:51:56 +0430

    Implementing CustomAuthTokenAPIView

--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -107,3 +107,20 @@ class ChangePasswordAPIView(generics.GenericAPIView):
             self.object.save()
             return Response({"details": "Password changed successfully."}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomAuthTokenAPIView(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username,
+            'email': user.email
+        })
