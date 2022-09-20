b407a349074bc1abec1bd070f3e2100b7068bfa2
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-20 11:24:55 +0430

    Implementing necessary fixtures

--- todo/api/tests/test_todo_api_views.py
+++ todo/api/tests/test_todo_api_views.py
@@ -2,3 +2,15 @@ import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+
+
+@pytest.fixture()
+def api_client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture()
+def common_user():
+    user = User.objects.create_user(username="test_user", password="A@123456")
+    return user
