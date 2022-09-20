bd8511abaa729a4ddae2a8fae8aa16066d26f771
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-20 13:33:28 +0430

    Implementing email message fixture

--- accounts/api/tests.py
+++ accounts/api/tests.py
@@ -1,3 +1,14 @@
 import pytest
 from mail_templated import EmailMessage
 from .utilities import EmailThread
+
+
+@pytest.fixture()
+def email_message():
+    email_obj = EmailMessage(
+        template_name="email/activation_email.tpl",
+        context={"token": "token"},
+        from_email="from@admin.com",
+        to=["to@user.com"],
+    )
+    return email_obj
