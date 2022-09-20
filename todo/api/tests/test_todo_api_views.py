9bbe274c7928fa2d8c91fb13c395eb8518a8979d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-20 12:47:48 +0430

    Implementing DetailAPIView tests

--- todo/api/tests/test_todo_api_views.py
+++ todo/api/tests/test_todo_api_views.py
@@ -61,3 +61,53 @@ class TestListAPIView:
         api_client.force_login(user=common_user)
         response = api_client.post(url, data)
         assert response.status_code == 400
+
+
+@pytest.mark.django_db
+class TestDetailAPIView:
+    def test_sample_task_fields(self, sample_task, common_user):
+        assert sample_task.title == "sample task"
+        assert sample_task.completed == False
+        assert sample_task.user == common_user
+
+    def test_retrieve_task_anonymous_user_status(self, api_client, sample_task):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        response = api_client.get(url)
+        assert response.status_code == 401
+
+    def test_retrieve_task_logged_in_user_status(self, api_client, common_user, sample_task):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        api_client.force_login(user=common_user)
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+    def test_update_task_anonymous_user_status(self, api_client, sample_task):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        data = {"title": "sample task edited", "completed": True}
+        response = api_client.put(url, data)
+        assert response.status_code == 401
+
+    def test_update_task_logged_in_user_status(self, api_client, sample_task, common_user):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        data = {"title": "sample task edited", "completed": True}
+        api_client.force_login(user=common_user)
+        response = api_client.put(url, data)
+        assert response.status_code == 200
+
+    def test_update_task_logged_in_user_invalid_data_status(self, api_client, sample_task, common_user):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        data = {"name": "task", "complete": "yes"}
+        api_client.force_login(user=common_user)
+        response = api_client.put(url, data)
+        assert response.status_code == 400
+
+    def test_destroy_task_anonymous_user_status(self, api_client, sample_task):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        response = api_client.delete(url)
+        assert response.status_code == 401
+
+    def test_destroy_task_logged_in_user_status(self, api_client, sample_task, common_user):
+        url = reverse("todo:todo_api:task_detail", kwargs={"todo_id": sample_task.id})
+        api_client.force_login(user=common_user)
+        response = api_client.delete(url)
+        assert response.status_code == 204
