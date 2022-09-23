e70e54a9813867b711806c526899685ad20b82a0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-23 13:59:02 +0330

    Implementing insert_task command class

--- todo/management/commands/insert_task.py
+++ todo/management/commands/insert_task.py
@@ -3,3 +3,25 @@ from faker import Faker
 from django.contrib.auth.models import User
 from todo.models import Task
 from random import choice
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        super(Command, self).__init__()
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(
+            username=self.fake.first_name(),
+            email=self.fake.email(),
+            first_name=self.fake.first_name(),
+            last_name=self.fake.last_name(),
+            password="Test@123456",
+        )
+
+        for _ in range(5):
+            Task.objects.create(
+                user=user,
+                title=self.fake.paragraph(nb_sentences=1),
+                completed=choice([True, False]),
+            )
