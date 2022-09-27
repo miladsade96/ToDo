f4b57a7775b4a0cefb302ed817e476454f245640
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-27 22:57:47 +0330

    Adding redis cache configuration

--- core/settings.py
+++ core/settings.py
@@ -163,3 +163,15 @@ CELERY_BROKER_URL = "redis://todo-redis:6379/1"  # pattern: redis://redis_ip_or_
 # celery start command inside docker compose --> celery -A core worker -l INFO
 # celery beat start command inside backend service -->
 # celery -A core  beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
+
+# Redis caching configurations
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://todo-redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
