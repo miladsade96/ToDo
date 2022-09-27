811f62c56d70a89e20a7cf554c3e94472c47e57b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-27 22:58:21 +0330

    Defining weather url path

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TodoListAPIView, TodoDetailApiView
+from .views import TodoListAPIView, TodoDetailApiView, WeatherApiView
 
 app_name = "todo_api"
 
 urlpatterns = [
     path("task-list/", TodoListAPIView.as_view(), name="task_list"),
     path("task-detail/<int:todo_id>/", TodoDetailApiView.as_view(), name="task_detail"),
+    path("weather/", WeatherApiView.as_view(), name="weather"),
 ]
