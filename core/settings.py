d936da9010385eca29ffbee4e7c5dbb3289ee521
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-28 22:47:45 +0330

    Changing debug and allowed hosts status

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(6wm78okjkjh&#uhirf^yaz*^-b@!o$wu9tp(2coxtqz6w0shi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
