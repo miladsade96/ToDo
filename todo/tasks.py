2f91bef162ec7a3f6dbc208e30bfe7e6722c6430
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-29 21:25:22 +0330

    Commenting unnecessary shared task

--- todo/tasks.py
+++ todo/tasks.py
@@ -1,7 +1,5 @@
 from celery import shared_task
 from .models import Task
-import requests
-from django.core.cache import cache
 
 
 @shared_task
@@ -9,12 +7,12 @@ def remove_completed_tasks():
     Task.objects.filter(completed=True).delete()
 
 
-@shared_task
-def check_weather():
-    response = requests.get(
-        "https://fd99998d-3122-4526-8338-6b73d7c9d79a.mock.pstmn.io/weather"
-    )
-    cache.set(
-        "weather", response.json(), timeout=1500
-    )  # time out is 25 minutes but will be overwritten every 20 minutes
-    return cache.get("weather")
+# @shared_task
+# def check_weather():
+#     response = requests.get(
+#         "https://fd99998d-3122-4526-8338-6b73d7c9d79a.mock.pstmn.io/weather"
+#     )
+#     cache.set(
+#         "weather", response.json(), timeout=1500
+#     )  # time out is 25 minutes but will be overwritten every 20 minutes
+#     return cache.get("weather")
